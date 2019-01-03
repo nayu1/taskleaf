6c3b518a4341d281e34a59ea467eb63ab6c161ad
Yukke Kamisuna <yukke5222@gmail.com>
2019-01-04 02:42:25 +0900

    add ransack

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,15 @@ class Task < ApplicationRecord
 
   belongs_to :user
 
-  scope :recent, -> { order(created_at: :desc) } 
+  scope :recent, -> { order(created_at: :desc) }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def ransackable_associations(auth_object = nil)
+    []
+  end
 
   private
 
